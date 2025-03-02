dcde65fe860cc3c66dc012c4e3b4a19602cb6582
davideganna <52606991+davideganna@users.noreply.github.com>
2025-03-02 16:39:34 +0100

    changed package installation

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
+# pip uninstall datamorphers -y
+# pip install -e .
+
+import subprocess
+import sys
 from setuptools import setup, find_packages
+
+package_name = "datamorphers"
+
+subprocess.run(
+    [sys.executable, "-m", "pip", "uninstall", "-y", package_name], check=False
+)
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="datamorphers",
+    name=package_name,
     version="0.1",
     packages=find_packages(),
     install_requires=requirements,
 )
+
+subprocess.run([sys.executable, "-m", "pip", "install", "-e", "."], check=True)
