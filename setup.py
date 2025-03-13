01032c17f61f51e018de091abfaf210e44f48545
davideganna <52606991+davideganna@users.noreply.github.com>
2025-03-13 17:25:06 +0100

    reverted setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 import subprocess
 import sys
+import pkg_resources
 from setuptools import setup, find_packages
 
 package_name = "datamorphers"
-github_url = "git+https://github.com/davideganna/DataMorph.git"
 
 # Check if the package is installed
-if subprocess.run(
-    [sys.executable, "-m", "pip", "show", package_name], capture_output=True, text=True
-).stdout:
-    print(f"Uninstalling {package_name}...")
-    subprocess.run([sys.executable, "-m", "pip", "uninstall", "-y", package_name])
+installed_packages = {pkg.key for pkg in pkg_resources.working_set}
+if package_name in installed_packages:
+    subprocess.run(
+        [sys.executable, "-m", "pip", "uninstall", "-y", package_name],
+        check=False,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
 
-# Install the package from GitHub
-print(f"Installing {package_name} from GitHub...")
-subprocess.run([sys.executable, "-m", "pip", "install", github_url])
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name=package_name,
+    version="0.1",
+    packages=find_packages(),
+    install_requires=requirements,
+)
